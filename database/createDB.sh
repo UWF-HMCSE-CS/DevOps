018495cdfa55ae0d131b0f90d7ad229d3ffd4f1a
Nathan Cooper <nac33@students.uwf.edu>
2016-11-10 19:51:43 -0600

    Fix paths

--- database/createDB.sh
+++ database/createDB.sh
@@ -7,4 +7,4 @@ sudo docker stop database
 
 sudo docker rm database
 
-sudo docker run -d --name database -p 3306:3306 -v /home/ec2-user/workspace/DevOps-Pipeline/database/:/pipeline/ --privileged sql_database bash /pipeline/setDB.sh
+sudo docker run -d --name database -p 3306:3306 -v /home/ec2-user/workspace/DevOps/database/:/pipeline/ --privileged sql_database bash /pipeline/setDB.sh
