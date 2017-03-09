c4af8064782d9eccd847bc00d4b3428801ae244a
Nathan Cooper <Nathan Cooper>
2017-03-08 21:16:57 -0600

    Fixed run command in docker

--- tests/java/staging/build.sh
+++ tests/java/staging/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 sudo docker build -t jstage /home/ec2-user/workspace/DevOps/tests/java/staging
-sudo docker run -itd -p 80:80 -privileged jstage
+sudo docker run -itd -p 80:80 --privileged jstage
