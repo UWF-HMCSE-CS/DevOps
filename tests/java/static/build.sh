5d581a4e816bdfe7d0a96f3fbee17d6d55aa214f
Nathan Cooper <Nathan Cooper>
2017-03-12 14:12:04 -0500

    Fix pathing issue

--- tests/java/static/build.sh
+++ tests/java/static/build.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-cp -r /home/ec2-user/workspace/Medium/MediumFX ./
-sudo docker build -t jstatic .
-sudo docker run -it jstatic
+rm -rf pmd-bin*
+wget https://sourceforge.net/projects/pmd/files/pmd/5.1.0/pmd-bin-5.1.0.zip
+unzip -q pmd-bin-5.1.0.zip
+cd pmd-bin-5.1.0/bin/
+bash run.sh pmd -dir /maven/MediumFX/src/application/ -f text -rulesets java-basic,java-design
