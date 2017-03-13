285e932b381b07d6c0f6d3ca6b5b0f7c19ff6b8c
EC2 Default User <ec2-user@ip-172-31-59-247.ec2.internal>
2017-03-13 23:32:51 +0000

    Fix unit tests and static test

--- tests/java/unit/localtest.sh
+++ tests/java/unit/localtest.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
-
 docker build -t junit /home/ec2-user/workspace/DevOps/tests/java/unit/ 
 docker run -t -d -v /home/ec2-user/workspace/jenkins_pipeline/medium:/maven --name "junit" junit
