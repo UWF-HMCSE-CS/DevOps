2b02011c5d6b9c0a4d162319a7376664e5c0c0eb
Nathan Cooper <Nathan Cooper>
2017-03-14 12:52:47 -0500

    fix static with cdep file system

--- tests/java/static/build.sh
+++ tests/java/static/build.sh
@@ -3,4 +3,4 @@ rm -rf pmd-bin*
 wget https://sourceforge.net/projects/pmd/files/pmd/5.1.0/pmd-bin-5.1.0.zip
 unzip -q pmd-bin-5.1.0.zip
 cd pmd-bin-5.1.0/bin/
-bash run.sh pmd -dir /maven/MediumFX/src/application/ -f text -rulesets java-basic,java-design
+bash run.sh pmd -dir /cdep/MediumFX/src/application/ -f text -rulesets java-basic,java-design
