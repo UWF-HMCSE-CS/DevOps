951d1c400c4f15afad27a81d0ccbd472edd79abb
Nathan Cooper <Nathan Cooper>
2017-03-15 21:56:27 -0500

    Fix merging build script

--- tests/staging/packaging/build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-(cd /pipeline/; mvn package)
-
-git checkout master
-git merge $1
-git push $2 master
--- tests/staging/stage/java_stage/merging/build.sh
+++ tests/staging/stage/java_stage/merging/build.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
 (cd /pipeline/; mvn package)
+
+git checkout master
+git merge $1
+git push $2 master
