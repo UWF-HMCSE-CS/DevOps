318a8f5b6938ec1e444ecd1cc04163290d729c69
Nathan Cooper <Nathan Cooper>
2017-03-16 19:27:56 -0500

    Add copy of jar in java.zip file

--- tests/staging/stage/java_stage/bash/push.sh
+++ tests/staging/stage/java_stage/bash/push.sh
@@ -2,6 +2,8 @@
 
 sudo docker exec -d pipeline_stage bash /pipeline/java_stage/bash/createDIR.sh $1
 sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/index/ pipeline_stage:/var/www/html/java_stage/$1/
+(cd  /home/ec2-user/workspace/jenkins_pipeline/$1; sudo zip java.zip /home/ec2-user/workspace/jenkins_pipeline/$1/target/*.jar)
+sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/java.zip pipeline_stage:/var/www/html/java_stage/$1/java.zip
 
 
 sudo docker cp /home/ec2-user/workspace/DevOps/tests/php/push/index.php pipeline_stage:/var/www/html/java_stage/$1/index.php
