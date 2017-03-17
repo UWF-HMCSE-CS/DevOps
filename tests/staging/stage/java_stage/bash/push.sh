6f98b0b77c9852a2ecfa750e05bbe8325e529294
Nathan Cooper <Nathan Cooper>
2017-03-16 21:04:14 -0500

    fix path

--- tests/staging/stage/java_stage/bash/push.sh
+++ tests/staging/stage/java_stage/bash/push.sh
@@ -2,7 +2,7 @@
 
 sudo docker exec -d pipeline_stage bash /pipeline/java_stage/bash/createDIR.sh $1
 sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/Index/index.php pipeline_stage:/var/www/html/java_stage/$1/index.php
-(cd  /home/ec2-user/workspace/jenkins_pipeline/$1; sudo zip java.zip ./*.jar)
+(cd  /home/ec2-user/workspace/jenkins_pipeline/$1/; sudo zip java.zip ./*.jar)
 sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/java.zip pipeline_stage:/var/www/html/java_stage/$1/java.zip
 
 
