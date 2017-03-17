ead1b70fddcaf81dff0288d339f85e872ec320ab
Nathan Cooper <Nathan Cooper>
2017-03-16 20:26:50 -0500

    Fix the path

--- tests/staging/stage/java_stage/bash/push.sh
+++ tests/staging/stage/java_stage/bash/push.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo docker exec -d pipeline_stage bash /pipeline/java_stage/bash/createDIR.sh $1
-sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/Index/index.html pipeline_stage:/var/www/html/java_stage/$1/index.html
+sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/Index/index.php pipeline_stage:/var/www/html/java_stage/$1/index.php
 (cd  /home/ec2-user/workspace/jenkins_pipeline/$1; sudo zip java.zip /home/ec2-user/workspace/jenkins_pipeline/$1/target/*.jar)
 sudo docker cp /home/ec2-user/workspace/jenkins_pipeline/$1/java.zip pipeline_stage:/var/www/html/java_stage/$1/java.zip
 
